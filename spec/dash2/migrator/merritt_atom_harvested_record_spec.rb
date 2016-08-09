7591136e8047b252fddbf95ad14f92957ca3ef18
David Moles <david.moles@ucop.edu>
2016-08-09 09:46:51 -0700

    Improve test for file list generation

--- spec/dash2/migrator/merritt_atom_harvested_record_spec.rb
+++ spec/dash2/migrator/merritt_atom_harvested_record_spec.rb
@@ -13,14 +13,14 @@ module Dash2
       end
 
       before(:each) do
-        mrt_mom_uri = 'https://merritt.cdlib.org/d/ark%3A%2Fc5146%2Fr3rg6g/1/system%2Fmrt-mom.txt'
+        mrt_mom_uri = 'https://merritt.cdlib.org/d/ark%3A%2Fc5146%2Fr36p4t/2/system%2Fmrt-mom.txt'
         stub_request(:get, mrt_mom_uri).to_return(body: File.read('spec/data/mrt-mom.txt'))
 
-        datacite_uri = 'https://merritt.cdlib.org/d/ark%3A%2Fc5146%2Fr3rg6g/1/producer%2Fmrt-datacite.xml'
+        datacite_uri = 'https://merritt.cdlib.org/d/ark%3A%2Fc5146%2Fr36p4t/2/producer%2Fmrt-datacite.xml'
         stub_request(:get, datacite_uri).to_return(body: File.read('spec/data/mrt-datacite.xml'))
 
         feed_uri = 'https://merritt.cdlib.org/object/recent.atom?collection=ark:/13030/m5709fmd'
-        entry_xml = File.read('spec/data/entry-r3rg6g.xml')
+        entry_xml = File.read('spec/data/entry-r36p4t.xml')
         entry = RSS::Parser.parse(entry_xml, false).items[0]
         @record = MerrittAtomHarvestedRecord.new(feed_uri, entry)
         @wrapper = record.as_wrapper
@@ -28,7 +28,7 @@ module Dash2
 
       describe '#identifier' do
         it 'extracts the identifier' do
-          expect(record.identifier).to eq('http://n2t.net/ark:/c5146/r3rg6g')
+          expect(record.identifier).to eq('http://n2t.net/ark:/c5146/r36p4t')
         end
       end
 
@@ -40,20 +40,94 @@ module Dash2
         end
 
         it 'extracts the file inventory' do
-          pathname = 'A_Zebrafish_Model_for_Studies_on_Esophageal_Epithelial_Biology.PDF'
-          size = 3_824_823
-          type = MIME::Type.new('application/pdf')
+
+          text_plain = MIME::Type.new('text/plain')
+          text_csv = MIME::Type.new('text/csv')
+          application_openxml = MIME::Type.new('application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+          application_pdf = MIME::Type.new('application/pdf')
+
+          expected = [
+              {
+                  title: 'producer/survey_Q11-23_sensors_platforms.R',
+                  length: 26414,
+                  type: text_plain
+              },
+              {
+                  title: 'producer/survey_Q25-32_data_metadata_management.R',
+                  length: 9794,
+                  type: text_plain
+              },
+              {
+                  title: 'producer/survey_Q10_research_sites.R',
+                  length: 1180,
+                  type: text_plain
+              },
+              {
+                  title: 'producer/clean_survey_data_no_ids.csv',
+                  length: 320890,
+                  type: text_csv
+              },
+              {
+                  title: 'producer/survey_Q3-9_respondent_info.R',
+                  length: 13784,
+                  type: text_plain
+              },
+              {
+                  title: 'producer/Laney_IRBProposal.docx',
+                  length: 1097848,
+                  type: application_openxml
+              },
+              {
+                  title: 'producer/survey_data_prep.R',
+                  length: 4359,
+                  type: text_plain
+              },
+              {
+                  title: 'producer/research_coords.csv',
+                  length: 5827,
+                  type: text_csv
+              },
+              {
+                  title: 'producer/Laney_300394_Exempt_Determination_Letter.pdf',
+                  length: 42686,
+                  type: application_pdf
+              },
+              {
+                  title: 'producer/survey_Q33-37_networking.R',
+                  length: 22658,
+                  type: text_plain
+              },
+              {
+                  title: 'producer/SensorSurvey_Printout.pdf',
+                  length: 1728537,
+                  type: application_pdf
+              },
+              {
+                  title: 'producer/survey_Q24_limitsToExpansion.R',
+                  length: 11012,
+                  type: text_plain
+              },
+              {
+                  title: 'producer/survey_Q38-42_publications.R',
+                  length: 1690,
+                  type: text_plain
+              }
+          ]
 
           inventory = wrapper.inventory
           expect(inventory).not_to be_nil
 
           files = inventory.files
-          expect(files.size).to eq(1)
+          expect(files.size).to eq(expected.size)
 
-          file = files[0]
-          expect(file.pathname).to eq(pathname)
-          expect(file.size_bytes).to eq(size)
-          expect(file.mime_type).to eq(type)
+          files.each_with_index do |file, i|
+            pathname = expected[i][:title].sub(/^producer\//, '')
+            size = expected[i][:length]
+            type = expected[i][:type]
+            expect(file.pathname).to eq(pathname)
+            expect(file.size_bytes).to eq(size)
+            expect(file.mime_type).to eq(type)
+          end
         end
 
         it 'extracts the license' do
