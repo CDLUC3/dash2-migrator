45c9f6203016c3b04e79fd0aece860cbd2022846
David Moles <david.moles@ucop.edu>
2016-08-18 16:42:36 -0700

    Add ucsf2 migrator

---
+++ migrate_ucsf2_to_ucop.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+# ############################################################
+# Includes / Requires
+
+lib_path = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib_path) unless $LOAD_PATH.include?(lib_path)
+
+require 'stash/harvester_app'
+require 'dash2/migrator'
+
+# ############################################################
+# Check environment
+
+ENV['STASH_ENV'] ||= 'test'
+raise 'Production migration not implemented' if ENV['STASH_ENV'] == 'production'
+
+# ############################################################
+# Configure users
+
+class Dash2::Migrator::MerrittAtomHarvestedRecord
+  def self.ucop_users
+    @ucop_users ||= begin
+      ucop_users = StashEngine::User.where(tenant_id: 'ucop').all
+      if ucop_users.empty?
+        [StashEngine::User.create(
+          uid: 'lmuckenhaupt-ucop@ucop.edu',
+          first_name: 'Lisa',
+          last_name: 'Muckenhaupt',
+          email: 'lmuckenhaupt@ucop.edu',
+          provider: 'developer',
+          tenant_id: 'ucop'
+        )]
+      else
+        ucop_users
+      end
+    end
+  end
+
+  def self.next_uid_index
+    @uid_index ||= -1
+    next_index = @uid_index + 1
+    @uid_index = next_index < ucop_users.size ? next_index : 0
+  end
+
+  def self.next_uid
+    ucop_users[next_uid_index].uid
+  end
+
+  def user_uid
+    @user_uid ||= Dash2::Migrator::MerrittAtomHarvestedRecord.next_uid
+  end
+end
+
+# ############################################################
+# Harvest
+
+config_file = 'config/migrator-ucsf-to-ucop.yml'
+app = Stash::HarvesterApp::Application.with_config_file(config_file)
+app.start
