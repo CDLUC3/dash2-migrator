d659e9bb756323116f6c51ccc3b4c115340c002e
David Moles <david.moles@ucop.edu>
2016-08-19 14:00:04 -0700

    Test multiple collections with re-harvesting

--- spec/integration/stash/harvester_app/application_spec.rb
+++ spec/integration/stash/harvester_app/application_spec.rb
@@ -38,18 +38,33 @@ module Stash
         allow(Stash::Sword::Client).to receive(:new) { @sword_client }
       end
 
-      configs = {
-          'config/migrator-ucsf2-to-ucop.yml' => 22,
-          'config/migrator-ucsf-to-ucop.yml' => 8,
-          'config/migrator-dataone-to-ucop.yml' => 17,
-      }
+      it 'harvests and imports' do
+        configs = {
+            'config/migrator-ucsf2-to-ucop.yml' => 22,
+            'config/migrator-ucsf-to-ucop.yml' => 8,
+            'config/migrator-dataone-to-ucop.yml' => 17,
+        }
+        total = configs.values.inject(0) { |sum, x| sum + x }
 
-      configs.each do |config_file, expected_count|
-        it "harvests and imports #{config_file}" do
+        expect(StashEngine::Resource.count).to eq(0) # just to be sure
+
+        configs.each do |config_file, expected_count|
+          before_count = StashEngine::Resource.count
           Stash::HarvesterApp::Application.with_config_file(config_file).start
-          expect(StashEngine::Resource.count).to eq(expected_count)
+          after_count = StashEngine::Resource.count
+          added_count = after_count - before_count
+          expect(added_count).to eq(expected_count)
         end
+
+        expect(StashEngine::Resource.count).to eq(total) # just to be sure
+
+        # Check reimports
+        configs.keys.each do |config_file|
+          Stash::HarvesterApp::Application.with_config_file(config_file).start
+        end
+        expect(StashEngine::Resource.count).to eq(total) # just to be sure
       end
+
     end
   end
 end
