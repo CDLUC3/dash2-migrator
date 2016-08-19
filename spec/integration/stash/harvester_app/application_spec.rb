cc072fc34b55871628a2146c9902d10ea32827c3
David Moles <david.moles@ucop.edu>
2016-08-19 12:54:33 -0700

    Test multiple collections

--- spec/integration/stash/harvester_app/application_spec.rb
+++ spec/integration/stash/harvester_app/application_spec.rb
@@ -8,50 +8,47 @@ module Stash
       attr_reader :user_uid
 
       before(:all) do
-        @user_uid = 'lmuckenhaupt-dataone@ucop.edu'
-        @em_count = 0
-        @edit_count = 0
+        @user_uid = 'lmuckenhaupt-ucop@ucop.edu'
       end
 
       before(:each) do
         @user = StashEngine::User.create(
-          uid: user_uid,
-          first_name: 'Lisa',
-          last_name: 'Muckenhaupt',
-          email: 'lmuckenhaupt@example.org',
-          provider: 'developer',
-          tenant_id: 'dataone'
+            uid: user_uid,
+            first_name: 'Lisa',
+            last_name: 'Muckenhaupt',
+            email: 'lmuckenhaupt@ucop.edu',
+            provider: 'developer',
+            tenant_id: 'ucop'
         )
         allow_any_instance_of(Dash2::Migrator::MerrittAtomHarvestedRecord).to receive(:user_uid) { user_uid }
 
         @ezid_client = instance_double(StashEzid::Client)
-        allow(@ezid_client).to receive(:mint_id) {
+        allow(@ezid_client).to receive(:mint_id) do
           time = Time.now
-          "doi:10.5072/FK#{time.to_i}.#{time.nsec}"
-        }
+          @last_doi = "doi:10.5072/FK#{time.to_i}.#{time.nsec}"
+        end
         allow(StashEzid::Client).to receive(:new) { @ezid_client }
 
         receipt = instance_double(Stash::Sword::DepositReceipt)
-        allow(receipt).to receive(:em_iri) do
-          @em_count += 1
-          "http://example.org/#{@em_count}/em"
-        end
-        allow(receipt).to receive(:edit_iri) do
-          @edit_count += 1
-          "http://example.org/#{@edit_count}/edit"
-        end
+        allow(receipt).to(receive(:em_iri)) { "http://example.org/#{@last_doi}/em" }
+        allow(receipt).to(receive(:edit_iri)) { "http://example.org/#{@last_doi}/edit" }
 
         @sword_client = instance_double(Stash::Sword::Client)
         allow(@sword_client).to receive(:create) { receipt }
         allow(Stash::Sword::Client).to receive(:new) { @sword_client }
       end
 
-      it 'harvests and imports' do
-        config_file = 'config/migrator-dataone.yml'
-        app = Stash::HarvesterApp::Application.with_config_file(config_file)
-        app.start
+      configs = {
+          'config/migrator-ucsf2-to-ucop.yml' => 22,
+          'config/migrator-ucsf-to-ucop.yml' => 8,
+          'config/migrator-dataone-to-ucop.yml' => 17,
+      }
 
-        expect(StashEngine::Resource.count).to eq(17)
+      configs.each do |config_file, expected_count|
+        it "harvests and imports #{config_file}" do
+          Stash::HarvesterApp::Application.with_config_file(config_file).start
+          expect(StashEngine::Resource.count).to eq(expected_count)
+        end
       end
     end
   end
