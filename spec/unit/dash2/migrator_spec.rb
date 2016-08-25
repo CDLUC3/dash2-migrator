73c5268961fba1ec19ba6d3bc2f4c0e5e5af8a68
David Moles <david.moles@ucop.edu>
2016-08-25 15:57:28 -0700

    Added pending tests

--- spec/unit/dash2/migrator_spec.rb
+++ spec/unit/dash2/migrator_spec.rb
@@ -2,5 +2,8 @@ module Dash2
   describe Migrator do
     it 'writes stash-wrapper dates as Zulu'
     it 'writes the modified datacite XML back into the stash wrapper'
+    it 'collapses whitespace'
+    it 'trims'
+    it 'dehyphenates' # gsub(/-\s+/, '')
   end
 end
