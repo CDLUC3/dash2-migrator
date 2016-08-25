dd05680c96061a4337296befb6bfcfe33271b5b9
David Moles <david.moles@ucop.edu>
2016-08-25 10:56:45 -0700

    See if we can get Travis to pick up local dependencies

---
+++ travis-local-deps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir ../stash_engines
+git clone git@github.com:CDLUC3/stash_engine.git ../stash_engines/stash_engine
+git clone git@github.com:CDLUC3/stash_datacite.git ../stash_engines/stash_datacite
+git clone git@github.com:CDLUC3/stash-harvester.git ../stash-harvester
