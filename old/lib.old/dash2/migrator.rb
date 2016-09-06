b567c5a3bdfe30792aaf65f8e0935a24ca0b8253
David Moles <david.moles@ucop.edu>
2016-09-06 09:31:16 -0700

    Remove old code

--- old/lib.old/dash2/migrator.rb
+++
@@ -1,8 +0,0 @@
-require 'active_support'
-require 'logger'
-
-module Dash2
-  module Migrator
-    Dir.glob(File.expand_path('../migrator/*.rb', __FILE__)).sort.each(&method(:require))
-  end
-end
--- old/spec/db/dash2/migrator/dash2_indexer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'db_spec_helper'
-
-module Dash2
-  module Migrator
-    describe Dash2Indexer do
-      it 'indexes a test record'
-    end
-  end
-end
