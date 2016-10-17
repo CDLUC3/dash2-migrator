055f26c621a7c76121a0bf84dcdd252c53a8a38e
David Moles <david.moles@ucop.edu>
2016-10-17 09:19:05 -0700

    Cleaned up test

--- spec/unit/eml/mapping/dataset_spec.rb
+++ spec/unit/eml/mapping/dataset_spec.rb
@@ -8,7 +8,9 @@ module Eml
       it 'round-trips a file' do
         f = 'spec/data/eml/dash1-eml-xml/dataone-ark+=90135=q1057cwm-mrt-eml.xml'
         eml_xml = File.binread(f)
-        fail "Unable to read file #{f}" unless eml_xml && !eml_xml.strip == ''
+
+        expect(eml_xml).not_to be_nil, "File.binread('#{f}') returned nil"
+        expect(eml_xml.strip).not_to eq(''), "File.binread('#{f}') returned blank"
 
         filtered_xml = Mapping.filter(eml_xml)
 
@@ -23,7 +25,9 @@ module Eml
         aggregate_failures 'all files' do
           Dir.glob('spec/data/eml/dash1-eml-xml/*.xml').sort.each do |f|
             eml_xml = File.binread(f)
-            fail "Unable to read file #{f}" unless eml_xml && !eml_xml.strip == ''
+
+            expect(eml_xml).not_to be_nil, "File.binread('#{f}') returned nil"
+            expect(eml_xml.strip).not_to eq(''), "File.binread('#{f}') returned blank"
 
             filtered_xml = Mapping.filter(eml_xml)
 
