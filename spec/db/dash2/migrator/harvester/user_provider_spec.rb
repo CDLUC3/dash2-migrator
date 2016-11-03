21a2212d10e6a4f9fea70c3ec0ae1a1437885bd3
David Moles <david.moles@ucop.edu>
2016-11-03 14:56:48 -0700

    Use Deardorff instead of Laurance for UCSF datasets

--- spec/db/dash2/migrator/harvester/user_provider_spec.rb
+++ spec/db/dash2/migrator/harvester/user_provider_spec.rb
@@ -18,14 +18,11 @@ module Dash2
 
         describe '#stash_user_id_for' do
           it 'maps the users' do
-            records.each do |r|
-              dash1_user = user_provider.dash1_user_for(local_id: r.local_id, title: r.title)
-              stash_user_id = user_provider.stash_user_id_for(local_id: r.local_id, title: r.title)
-              expect(stash_user_id).to be_nil unless dash1_user
-              expect(stash_user_id).not_to be_nil if dash1_user
+            records.each do |record|
+              stash_user_id = user_provider.stash_user_id_for(record)
+              expect(stash_user_id).not_to be_nil
             end
-
-            expect(StashEngine::User.count).to eq(68)
+            expect(StashEngine::User.count).to eq(72)
           end
         end
       end
