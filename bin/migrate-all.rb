155f80094bb9d71d8ca2f301c566b2d3e79cf478
David Moles <david.moles@ucop.edu>
2016-11-07 12:11:59 -0800

    Use real user configuration

--- bin/migrate-all.rb
+++ bin/migrate-all.rb
@@ -16,50 +16,9 @@ $LOAD_PATH.unshift(lib_path) unless $LOAD_PATH.include?(lib_path)
 require 'dash2/migrator'
 
 # ############################################################
-# Configure users
-
-class Dash2::Migrator::Harvester::MerrittAtomHarvestedRecord
-  def self.ucop_users
-    @ucop_users ||= begin
-      ucop_users = StashEngine::User.where(tenant_id: 'ucop').all
-      if ucop_users.empty?
-        [create_default_user!]
-      else
-        ucop_users
-      end
-    end
-  end
-
-  def self.create_default_user!
-    StashEngine::User.create(
-      uid: 'lmuckenhaupt-ucop@ucop.edu',
-      first_name: 'Lisa',
-      last_name: 'Muckenhaupt',
-      email: 'lmuckenhaupt@ucop.edu',
-      provider: 'developer',
-      tenant_id: 'ucop'
-    )
-  end
-
-  def self.next_uid_index
-    @uid_index ||= -1
-    next_index = @uid_index + 1
-    @uid_index = next_index < ucop_users.size ? next_index : 0
-  end
-
-  def self.next_uid
-    ucop_users[next_uid_index].uid
-  end
-
-  def user_uid
-    @user_uid ||= Dash2::Migrator::Harvester::MerrittAtomHarvestedRecord.next_uid
-  end
-end
-
-# ############################################################
 # Migrate
 
-job = Dash2::Migrator::MigrationJob.from_file('config/migrate-all-to-ucop.yml')
+job = Dash2::Migrator::MigrationJob.from_file('config/migrate-all.yml')
 job.migrate!
 
 Dash2::Migrator::Importer::Importer.clean_up!
