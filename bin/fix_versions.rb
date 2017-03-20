416cfc1f1225586393d94a3299a22f4f4609c762
David Moles <david.moles@ucop.edu>
2017-03-20 16:22:13 -0700

    Generated version lists for dev and stage

--- bin/fix_versions.rb
+++ bin/fix_versions.rb
@@ -26,42 +26,41 @@ config_file = case stash_env
     'config/migrate-all.yml'
 end
 
-job = MigrationJob.from_file(config_file)
-job.sources.each do |source|
-  tenant_path = source[:tenant_path]
-  feed_uri = source[:feed_uri]
-  config = Harvester::MerrittAtomSourceConfig.new(
-    tenant_path: tenant_path,
-    feed_uri: feed_uri,
-    user_provider: nil,
-    env_name: stash_env
-  )
-
-  tenant = config.tenant_id
-  puts "# #{tenant}\t#{feed_uri}"
-
-  puts "ark\tdoi\tstash_version\tmerritt_version"
-
-  harvest_task = Harvester::MerrittAtomHarvestTask.new(config: config)
-  harvest_task.harvest_records.each do |record|
-    ark = record.ark
-    next unless ark
-
-    doi = record.doi
-    stash_version = record.stash_version
-    merritt_version = record.merritt_version
-    puts "#{ark}\t#{doi}\t#{stash_version}\t#{merritt_version}"
-
-    if stash_version && (stash_version != merritt_version)
-      diffs << {
-        tenant: tenant,
-        doi: doi,
-        stash_version: stash_version,
-        merritt_version: merritt_version,
-      }
-    end
-  end
-end
-
-puts diffs.to_yaml
-
+# job = MigrationJob.from_file(config_file)
+# job.sources.each do |source|
+#   tenant_path = source[:tenant_path]
+#   feed_uri = source[:feed_uri]
+#   config = Harvester::MerrittAtomSourceConfig.new(
+#     tenant_path: tenant_path,
+#     feed_uri: feed_uri,
+#     user_provider: nil,
+#     env_name: stash_env
+#   )
+#
+#   tenant = config.tenant_id
+#   puts "# #{tenant}\t#{feed_uri}"
+#
+#   puts "ark\tdoi\tstash_version\tmerritt_version"
+#
+#   harvest_task = Harvester::MerrittAtomHarvestTask.new(config: config)
+#   harvest_task.harvest_records.each do |record|
+#     ark = record.ark
+#     next unless ark
+#
+#     doi = record.doi
+#     stash_version = record.stash_version
+#     merritt_version = record.merritt_version
+#     puts "#{ark}\t#{doi}\t#{stash_version}\t#{merritt_version}"
+#
+#     if stash_version && (stash_version != merritt_version)
+#       diffs << {
+#         tenant: tenant,
+#         doi: doi,
+#         stash_version: stash_version,
+#         merritt_version: merritt_version,
+#       }
+#     end
+#   end
+# end
+#
+# puts diffs.to_yaml
