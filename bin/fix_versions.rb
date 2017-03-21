786fb7a3e437feb8c7a22e09cd51c1ad9ed11514
David Moles <david.moles@ucop.edu>
2017-03-21 09:35:01 -0700

    First attempt at actual fix

--- bin/fix_versions.rb
+++ bin/fix_versions.rb
@@ -26,41 +26,45 @@ config_file = case stash_env
     'config/migrate-all.yml'
 end
 
-# job = MigrationJob.from_file(config_file)
-# job.sources.each do |source|
-#   tenant_path = source[:tenant_path]
-#   feed_uri = source[:feed_uri]
-#   config = Harvester::MerrittAtomSourceConfig.new(
-#     tenant_path: tenant_path,
-#     feed_uri: feed_uri,
-#     user_provider: nil,
-#     env_name: stash_env
-#   )
-#
-#   tenant = config.tenant_id
-#   puts "# #{tenant}\t#{feed_uri}"
-#
-#   puts "ark\tdoi\tstash_version\tmerritt_version"
-#
-#   harvest_task = Harvester::MerrittAtomHarvestTask.new(config: config)
-#   harvest_task.harvest_records.each do |record|
-#     ark = record.ark
-#     next unless ark
-#
-#     doi = record.doi
-#     stash_version = record.stash_version
-#     merritt_version = record.merritt_version
-#     puts "#{ark}\t#{doi}\t#{stash_version}\t#{merritt_version}"
-#
-#     if stash_version && (stash_version != merritt_version)
-#       diffs << {
-#         tenant: tenant,
-#         doi: doi,
-#         stash_version: stash_version,
-#         merritt_version: merritt_version,
-#       }
-#     end
-#   end
-# end
-#
-# puts diffs.to_yaml
+job = MigrationJob.from_file(config_file)
+job.sources.each do |source|
+  tenant_path = source[:tenant_path]
+  feed_uri = source[:feed_uri]
+  config = Harvester::MerrittAtomSourceConfig.new(
+    tenant_path: tenant_path,
+    feed_uri: feed_uri,
+    user_provider: nil,
+    env_name: stash_env
+  )
+
+  tenant = config.tenant_id
+  puts "# #{tenant}\t#{feed_uri}"
+
+  harvest_task = Harvester::MerrittAtomHarvestTask.new(config: config)
+  harvest_task.harvest_records.each do |record|
+    ark = record.ark
+    next unless ark
+
+    doi = record.doi
+    last_merritt_version = record.merritt_version
+    last_stash_version = record.stash_version
+
+    # TODO: get this into something we can test
+
+    identifier = StashEngine::Identifier.find_by(identifier: doi)
+    unless identifier
+      Stash::Harvester.log.warn("No database record for identifier #{doi}; skipping")
+      next
+    end
+
+    diff = last_merritt_version - last_stash_version
+    if diff != 0
+      submitted = (resources = identifier.resources) && resources.submitted
+      submitted.each do |v|
+        v.merritt_version += diff
+        v.save!
+      end
+    end
+
+  end
+end
