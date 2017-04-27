44549185a5fdb88689d5cfbff81ee95e661832b2
David Moles <david.moles@ucop.edu>
2017-04-27 11:11:14 -0700

    Fix local dependencies path

--- travis-local-deps.sh
+++ travis-local-deps.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir ../stash_engines
-git clone https://github.com/CDLUC3/stash_engine.git ../stash_engines/stash_engine
-git clone https://github.com/CDLUC3/stash_datacite.git ../stash_engines/stash_datacite
-git clone https://github.com/CDLUC3/stash-harvester.git ../stash-harvester
+git clone https://github.com/CDLUC3/stash ../stash
 
